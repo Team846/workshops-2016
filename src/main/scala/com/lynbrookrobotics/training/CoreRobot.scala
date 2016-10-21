26e9785fad726e621ce4d5c71f9b66a2e1705913
Shadaj Laddad <shadaj@users.noreply.github.com>
2016-10-21 14:04:55 -0700

    Fix use of Funky Dashboard for plotting shooter data

--- src/main/scala/com/lynbrookrobotics/training/CoreRobot.scala
+++ src/main/scala/com/lynbrookrobotics/training/CoreRobot.scala
@@ -14,6 +14,9 @@ import com.lynbrookrobotics.funkydashboard.{FunkyDashboard, TimeSeriesNumeric}
 import com.lynbrookrobotics.training.shooterArm.tasks.ShooterArmPositionControl
 import squants.space.Degrees
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
 class CoreRobot(implicit config: RobotConfig, clock: Clock) {
   implicit val driverHardware = config.driver.asHardware
   implicit val shooterArmConfig = config.shooterArm
@@ -25,15 +28,18 @@ class CoreRobot(implicit config: RobotConfig, clock: Clock) {
   driverHardware.operatorJoystick.buttonPressed(1).foreach(new ShooterArmPositionControl(Degrees(20)))
   driverHardware.operatorJoystick.buttonPressed(2).foreach(new ShooterArmPositionControl(Degrees(60)))
 
-  FunkyDashboard.datasetGroup("shooter-arm").addDataset(new TimeSeriesNumeric("Pot Angle")(
+  val dashboard = Future {
+    val dash = new FunkyDashboard
+    implicit val system = ActorSystem("funky-dashboard")
+    implicit val materializer = ActorMaterializer()
+    implicit val routingSettings = RoutingSettings.default
+
+    Http().bindAndHandle(Route.handlerFlow(dash.route), "0.0.0.0", 8080)
+
+    dash
+  }
+
+  dashboard.foreach(_.datasetGroup("shooter-arm").addDataset(new TimeSeriesNumeric("Pot Angle")(
     shooterArmConfig.shooterArmAngle.get.to(Degrees)
-  ))
-
-  implicit val system = ActorSystem("funky-dashboard")
-  implicit val materializer = ActorMaterializer()
-  implicit val executionContext = system.dispatcher
-  implicit val routingSettings = RoutingSettings.default
-
-  Http().bindAndHandle(Route.handlerFlow(FunkyDashboard.route), "0.0.0.0", 8080)
-
+  )))
 }
